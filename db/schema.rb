4dea047456016d165546ee6cee99cfce09254f40
Nurbol-Xan <nurbolxaydaraliyev007@gmail.com>
2022-05-21 20:15:39 +0500

    third

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_125542) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_21_145724) do
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
