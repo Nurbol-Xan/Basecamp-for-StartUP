d7fbcfc9e1d4273fd5e0512d2b362baaf5429254
Nurbol-Xan <nurbolxaydaraliyev007@gmail.com>
2022-05-22 16:04:51 +0500

    success

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_152704) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_22_102757) do
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "views", default: 0
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_21_152704) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: 'nurbol@gmail.com', password: "password", password_confirmation: "password")
+
+3.times do |n|
+    Post.create(name: "Pro #{n}", description: "Con #{n}", user_id: User.first.id)
+end
