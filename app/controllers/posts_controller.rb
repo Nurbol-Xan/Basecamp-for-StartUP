11acf2953f26b95a363814a8145086f4aa6e9008
Nurbol-Xan <nurbolxaydaraliyev007@gmail.com>
2022-06-07 17:53:13 +0500

    ex.5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: %i[show index]
   # GET /posts or /posts.json
+
+
   def index
     @posts = Post.all.order(created_at: :desc)
   end
