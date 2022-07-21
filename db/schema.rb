28e7c877849154582ba4ede580d0e447c26c494c
Student astrum <student@astrum.domain>
2022-07-21 08:46:05 +0500

    task: continues, finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_19_184844) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_19_235955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_19_184844) do
     t.datetime "updated_at", null: false
     t.index ["discussion_id"], name: "index_answers_on_discussion_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "attachments", force: :cascade do |t|
+    t.string "content"
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_attachments_on_post_id"
+    t.index ["user_id"], name: "index_attachments_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -141,6 +151,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_19_184844) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "discussions"
   add_foreign_key "answers", "users"
+  add_foreign_key "attachments", "posts"
+  add_foreign_key "attachments", "users"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "discussions", "posts"
